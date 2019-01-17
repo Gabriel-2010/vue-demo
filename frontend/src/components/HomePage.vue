b7aeb20b292dd11d20acef22f4c330ae87ada750
Gabriel <pkguowu@gmail.com>
2019-01-17 16:52:27 +0800

    init project

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg2 }}</h1>
+    <h1><router-link to="/subpage">Go to Sub Page</router-link></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld3',
+  data () {
+    return {
+      msg2: 'Welcome to Home Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
