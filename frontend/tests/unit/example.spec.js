66e860808f1366b55dcf80aad6c13ea0e180dad8
Gabriel <pkguowu@gmail.com>
2019-01-25 14:49:12 +0800

    update test case

--- frontend/tests/unit/example.spec.js
+++ frontend/tests/unit/example.spec.js
@@ -1,11 +1,22 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/HomePage.vue'
+import SubPage from '@/components/SubPage.vue'
 
-describe('HelloWorld.vue', () => {
+describe('HomePage.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'Welcome to Home Page'
+    const wrapper = shallowMount(HomePage, {
+      propsData: { msg }
+    })
+    expect(wrapper.text()).to.include(msg)
+  })
+})
+
+describe('SubPage.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'Welcome to Sub Page'
+    const wrapper = shallowMount(SubPage, {
       propsData: { msg }
     })
     expect(wrapper.text()).to.include(msg)
